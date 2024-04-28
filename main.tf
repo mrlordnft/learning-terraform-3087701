8cdb107c38e9ab6e0aa93d55aaa3bab7f976c392
mrlordnft <cristiangogosila@gmail.com>
2024-04-28 16:24:16 +0300

    fix

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "alb" {
 
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
 
   target_groups = [
@@ -72,16 +72,11 @@ module "alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port = 80
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
       protocol = "HTTP"
       target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Environment = "Development"
   }
 }
 
